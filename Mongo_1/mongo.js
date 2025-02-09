3b3cd719874566849ca56af3b91b1d01b83cc299
daredevil9215 <grgo.lovric@gmail.com>
2025-02-09 13:28:45 +0200

    Added std and mean task 7

--- Mongo_1/mongo.js
+++ Mongo_1/mongo.js
@@ -235,47 +235,70 @@ async function main() {
         const collection = db.collection('weather_dataset');
 
         // 1. Updating missing values
-        console.log("Finding missing values...");
+        console.log("1. Finding missing values...");
         const missing = await getMissingValues(collection);
 
         if(missing.length > 0) {
-
-            for(let doc of missing) {
-                console.log(doc);
-            }
             console.log("Updating missing values.")
             await updateMissingValues(collection);
-
         }
         else {
             console.log("No missing values.");
         }
 
         // 2. Finding mean, standard deviation, creating a new document
-        console.log("Creating statistics for the dataset.");
+        console.log("2. Creating statistics for the dataset.");
         const statistics = db.collection("statistika_weather_dataset");
-        //await createStatisticsContinuous(collection, statistics);
+        await createStatisticsContinuous(collection, statistics);
 
         // 3. Calculating frequencies of categorical variables
-        console.log("Creating frequencies for the dataset.");
+        console.log("3. Creating frequencies for the dataset.");
         const frequencies = db.collection("frekvencije_weather_dataset");
-        //await createFrequenciesCategorical(collection, frequencies);
+        await createFrequenciesCategorical(collection, frequencies);
 
         // 4. New documents with continuous values
-        console.log("Creating documents with values lesser than and greater than means.");
+        console.log("4. Creating documents with values lesser than and greater than means.");
         const statistics_1 = db.collection("statistika1_weather_dataset");
         const statistics_2 = db.collection("statistika2_weather_dataset");
-        //await createLessThanMeansContinuous(collection, statistics, statistics_1);
-        //await createGreaterThanMeansContinuous(collection, statistics, statistics_2);
+        await createLessThanMeansContinuous(collection, statistics, statistics_1);
+        await createGreaterThanMeansContinuous(collection, statistics, statistics_2);
 
         // 5. Embedding frequencies
+        console.log("5. Embedding frequencies.");
         const embed = db.collection("emb_weather_dataset");
         //await embedCategorical(collection, frequencies, embed);
 
         // 6. Embedding statistics
+        console.log("6. Embedding statistics.");
         const embed_2 = db.collection("emb2_weather_dataset");
-        //await embedContinuous(collection, statistics, embed_2);
+        await embedContinuous(collection, statistics, embed_2);
 
+        // 7. Std, means
+        console.log("7. Finding standard deviations 10% bigger than means.");
+        const embed_2_data = await embed_2.find().toArray();
+        const embed_2_statistics = embed_2_data[0]["statistics"];
+
+        for(let c of columnsContinuous) {
+            let data = embed_2_statistics[c];
+            let mean = data["mean"];
+            let std = data["std"];
+
+            if(std === 0) {
+                continue;
+            }
+            if(std > mean * 0.10) {
+                data["std&mean10%"] = mean;
+                embed_2_statistics[c] = data;
+            }
+        }
+
+        for(let doc of embed_2_data) {
+            await embed_2.updateOne(
+                {"_id": doc["_id"]},
+                {$set: {"statistics": embed_2_statistics}},
+                {upsert: true},
+            )
+        }
 
     } finally {
         if (client) {
